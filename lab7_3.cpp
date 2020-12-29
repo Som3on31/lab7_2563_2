273fb4f7c3aca2122dfa9900159adb1ab566ffdd
Som3on31 <Som3on31_kek@users.noreply.github.com>
2020-12-29 15:37:13 +0700

    Completed the program.

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,31 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int age,height,property;
+    string status;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age<=20){
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height>=175){
+            cout << "Enter your property: ";
+            cin >> property;
+            status = (property>69000000)? "MARRIED":"ONE-NIGHT-STAND" ;
+        }else status = (height>=160)? "FRIEND" : "UNFRIEND" ;
+    }else if (age<30){
+        cout << "Enter your property: ";
+        cin >> property;
+        status = (property>10000000)? "BEST FRIEND":"UNFRIEND" ;
+    }else status="UNFRIEND";
+    
+    cout << "Your status = "<< status;
+
+    return 0;
+}
+
